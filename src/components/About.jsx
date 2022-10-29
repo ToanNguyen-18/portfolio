62971e2ff65e589ad16927d7d697cf2b32eaf92f
unknown <toannguyentat18@gmail.com>
2022-10-29 21:49:39 +0700

    changed 12

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,23 +14,32 @@ const About = () => {
         </div>
 
         <p className="text-xl">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+          I love codding and I love logic. I love threading and I love timer.
+          I love language and I love framework. I love frontend, backend and Api...
+          That's why I want to be a good Full Stack Developer. It's my dream since
+          I was at school. Starting as an Assembly language programer for 
+          8051 MCU families for over 5 years. Now, I came back to codding field 
+          for over 3 years as a Full Stack Developer.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+          I've run through 3 project so far in marketing, security and school fields.
+          Applying Deep Learing, writing API and database in python and django in server.
+          I can create desktop apps run on windows by PyQT5. Web apps run on browser by 
+          React, Boostrap or plain HTML, CSS and JavaScript. For Android I use Kotlin. 
+          I know OOPs and its 4 pillars like Polymorphism, Inheritance, Encapsulation and 
+          Abstraction.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          I am a self-taught man and always study by myself new framework and new language.
+          I completed Kotlin and Adroid Studio in 4 months and React in a month. I use React 
+          to write this portfolio Website hosting in Github pages. I am able to learn new 
+          things quickly and love to take use of available resource to achive the expecting result.
         </p>
       </div>
     </div>
