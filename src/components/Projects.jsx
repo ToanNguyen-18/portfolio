a3947783087ff7069c2db9151bc69a6289f46c0c
unknown <toannguyentat18@gmail.com>
2022-10-31 00:30:35 +0700

    c3

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -44,10 +44,10 @@ const Portfolio = () => {
           <p className="text-2xl font-bold inline border-b-4 border-gray-500">
             Projects
           </p>
-          <p className="py-6">Check out some of my work right here</p>
+          <p className="py-4 sm:py-6">Check out some of my work right here</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-8 px-0 sm:px-0">
           {portfolios.map(({ id, src }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
