a1ef8f494ed1736a533254c241ad6da1dbbfe4d3
unknown <toannguyentat18@gmail.com>
2022-10-30 21:38:55 +0700

    changed 18

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,23 +14,21 @@ const About = () => {
         </div>
 
         <p className="text-xl">
-          I love codding and I love logic. I love frontend, backend and Api. I love 
-          language and I love framework. That's why I am now a Full Stack Developer. 
+          I love codding and I love logic. I love frontend and I love backend. I love 
+          language and I love framework. That's why I am a Full Stack Developer. 
           Starting as an Assembly programer for 8051 MCU, now I know Python, Kotlin, 
-          JavaScript and Java. I also know OOPs and its 4 pillars charateristics. 
-          I use frameworks Django, React, and Spring and tools are Visual Code, 
-          PyCharm, Android Studio. I also know RESTful api and MySQL and PostgreSQL.
+          JavaScript and Java. I also know OOPs and frameworks Django, React, and Spring.
+          Tools are Visual Code, PyCharm, Android Studio. Api is RESTful and databases are MySQL and PostgreSQL.
         </p>
 
         <br />
 
         <p className="text-xl">
           As a Full Stack Developer I have to adapt to change in technology. I am a 
-          self-taught man who can learn new thing fast. Because of understanding OOps, 
-          I can change from one language and its framework to others in a short time 
-          as needed. Always, youtube and tutorial course are tool of mine for new 
-          technologies, new programming language and new framework. Currently I build
-          Web, Windows and Android applications.
+          self-taught man who can learn new thing fast. Understanding OOps helps me to 
+          change from one language and its framework to others in a short time. 
+          Always, youtube and tutorials are tool of mine for updating new 
+          technologies, language and framework. Currently, I build Web, Windows and Android applications.
         </p>
       </div>
     </div>
