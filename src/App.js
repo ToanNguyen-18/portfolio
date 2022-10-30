2f3e747c90fbc079d3bc58a52ce754e10b2e82e8
unknown <toannguyentat18@gmail.com>
2022-10-30 22:16:21 +0700

    changed 23

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Skills from "./components/Skills";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import Projects from "./components/Project";
+import Projects from "./components/Projects";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
