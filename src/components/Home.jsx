73245ee20b64d2b2d4caa72e3e4850722fda0efa
unknown <toannguyentat18@gmail.com>
2022-10-30 21:54:39 +0700

    changed 21

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,8 +11,8 @@ const Home = () => {
     >
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          <h2 className="text-3xl sm:text-7xl font-bold text-white">
-            Full Stack Developer
+          <h2 className="text-2xl sm:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I have over 3 years of experience in developing software. Currently, I build prontend apps using React, Tailwind and Sanity. 
