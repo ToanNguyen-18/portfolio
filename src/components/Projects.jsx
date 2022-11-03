fb9d8817bae36a6db075637f0b1b4c5cb6a56bb3
unknown <toannguyentat18@gmail.com>
2022-11-03 22:27:31 +0700

    9

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -52,7 +52,7 @@ const Portfolio = () => {
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div>
           <p className="text-2xl font-bold inline border-b-4 border-gray-500">
             Projects
           </p>
